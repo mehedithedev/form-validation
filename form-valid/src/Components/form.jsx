438fc101555f960c69c0734b5fc76229aa80ff8f
Mehedi Hasan <mehedi.developer@icloud.com>
2024-07-31 16:10:59 +0000

    fixed folder directories

---
+++ form-valid/src/Components/form.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import {useForm} from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+function Form() {
+   
+
+    const schema = yup.object().shape({
+        fullName: yup.string().required("Your full name is required"),
+        email: yup.string().email().required("You have to enter you email"),
+        mobileNumber: yup.number().integer(),
+        age: yup.number().positive().integer().min(18).required("Enter your age. Not below 18!"),
+        password: yup.string().min(4).max(32).required('Enter a password ranging from 4-32 digits'),
+        confirmPassword: yup
+        .string()
+        .oneOf([yup.ref('password'), null], "Password is not matching")
+        .required()        
+    })
+    const {register, handleSubmit, formState: {errors}} = useForm({
+        resolver: yupResolver(schema)
+    });
+
+    const onSubmit = (data)=>{
+        console.log(data)
+    }
+
+  return (
+    <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type='text' className='name' placeholder='Full Name...' {...register('fullName')}/>
+            <p>{errors.fullName?.message}</p>
+
+            <input type='text' className='email' placeholder='Email...' {...register('email')}/>
+            <p>{errors.email?.message}</p>
+            
+            <input type='text' className='phone' placeholder='Mobile Number...' {...register('mobileNumber')}/>
+            <p></p>
+            <input type='number' className='age' placeholder='Age...'{...register('age')}/>
+            <p>{errors.age?.message}</p>
+
+            <input type='password' className='password' placeholder='Password...' {...register('password')}/>
+            <p>{errors.password?.message}</p>
+
+            <input type='password' className='cnfpass' placeholder='Confirm Password...' {...register('confirmPassword')}/>
+            <p>{errors.confirmPassword?.message}</p>
+
+            <input type="submit"/>
+                    
+
+        </form>
+    </div>
+  )
+}
+
+export default Form
